5cc001b1467dfb0f9ac2047283c65ae35e1f3b1f
bRight <141350864+brightsablah@users.noreply.github.com>
2024-05-06 23:10:22 +0000

    Update push.c

--- push.c
+++ push.c
@@ -11,7 +11,7 @@ void push(stack_t **stack, unsigned int line_number)
     int value;
     stack_t *new_node = NULL;
 
-    if (arg == NULL || !isdigit(*arg))
+    if (arg == NULL || !isdigit(*arg) && *arg != '-' && *arg != '+')
     {
         fprintf(stderr, "L%u: usage: push integer\n", line_number);
         exit(EXIT_FAILURE);
