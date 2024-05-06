53d9381737b80d4113f26c5721c540cad5215b7f
bRight <141350864+brightsablah@users.noreply.github.com>
2024-05-06 22:44:51 +0000

    push function

---
+++ push.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * push - Pushes an element onto the stack
+ * @stack: Pointer to the top of the stack
+ * @line_number: Line number being executed
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+    char *arg = strtok(NULL, " \n");
+
+    if (arg == NULL || !isdigit(*arg))
+    {
+        fprintf(stderr, "L%u: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    int value = atoi(arg);
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = value;
+    new_node->prev = NULL;
+
+    if (*stack != NULL)
+        (*stack)->prev = new_node;
+
+    new_node->next = *stack;
+    *stack = new_node;
+}
