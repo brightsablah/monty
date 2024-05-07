13ce02fa4467ae3b6b3c86b29d2a52267836d3ad
bRight <141350864+brightsablah@users.noreply.github.com>
2024-05-07 03:07:30 +0000

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,19 +1,31 @@
 #include "monty.h"
 
 /**
- * pall - Prints all the values on the stack, starting from the top
- * @stack: Pointer to the top of the stack
+ * pall - Prints all the values on the stack or queue
+ * @stack: Pointer to the top of the stack or queue
  * @line_number: Line number being executed
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
+    stack_t *current;
+	(void)line_number;
 
-    (void)line_number;
-
-    while (current != NULL)
+    if (mode == 0) /* Stack mode (LIFO) */
     {
-        printf("%d\n", current->n);
-        current = current->next;
+        current = *stack;
+        while (current != NULL)
+        {
+            printf("%d\n", current->n);
+            current = current->next;
+        }
+    }
+    else if (mode == 1) /* Queue mode (FIFO) */
+    {
+        current = *stack;
+        while (current != NULL)
+        {
+            printf("%d\n", current->n);
+            current = current->prev;
+        }
     }
 }
