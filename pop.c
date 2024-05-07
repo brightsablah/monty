5852a992c153c3f41358438504ef55b9264a1bba
bRight <141350864+brightsablah@users.noreply.github.com>
2024-05-07 00:28:17 +0000

    pop opcode function

---
+++ pop.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * pop - Removes the top element of the stack
+ * @stack: Pointer to the top of the stack
+ * @line_number: Line number being executed
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    stack_t *temp = *stack;
+    *stack = (*stack)->next;
+    if (*stack != NULL)
+        (*stack)->prev = NULL;
+    free(temp);
+}
