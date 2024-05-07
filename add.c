737d1f45c624d3774b026514e0222440f510a703
bRight <141350864+brightsablah@users.noreply.github.com>
2024-05-07 00:41:16 +0000

    add opcode function

---
+++ add.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * add - Adds the top two elements of the stack
+ * @stack: Pointer to the top of the stack
+ * @line_number: Line number being executed
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    (*stack)->next->n += (*stack)->n;
+    pop(stack, line_number);
+}
