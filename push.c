e7c7bda6cf3db4c22ce2716207dd670292a1a014
bRight <141350864+brightsablah@users.noreply.github.com>
2024-05-07 02:37:27 +0000

    Update push.c

--- push.c
+++ push.c
@@ -11,6 +11,7 @@ void push(stack_t **stack, unsigned int line_number)
     long int value;
     stack_t *new_node = NULL;
     char *endptr;
+stack_t *last = NULL;
 
     if (arg == NULL || (!isdigit(*arg) && *arg != '-' && *arg != '+'))
     {
@@ -27,20 +28,41 @@ void push(stack_t **stack, unsigned int line_number)
         exit(EXIT_FAILURE);
     }
 
+/*
     value = atoi(arg);
+    */
     new_node = malloc(sizeof(stack_t));
     if (new_node == NULL)
     {
         fprintf(stderr, "Error: malloc failed\n");
         exit(EXIT_FAILURE);
     }
-
+    
     new_node->n = value;
     new_node->prev = NULL;
 
+    if (MODE == 0) /* Stack mode (LIFO) */
+    {
+        if (*stack != NULL)
+            (*stack)->prev = new_node;
+        new_node->next = *stack;
+        *stack = new_node;
+    }
+    else if (mode == 1) /* Queue mode (FIFO) */
+    {
+        last = *stack;
+        if (last != NULL)
+        {
+            while (last->next != NULL)
+                last = last->next;
+            last->next = new_node;
+            new_node->prev = last;
+        }
+/*
     if (*stack != NULL)
         (*stack)->prev = new_node;
 
     new_node->next = *stack;
     *stack = new_node;
+        */
 }
