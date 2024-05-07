c59411a296bf3f1dbf1606cf00e40590da32a6e0
bRight <141350864+brightsablah@users.noreply.github.com>
2024-05-07 01:20:26 +0000

    Create rotl.c

---
+++ rotl.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * rotl_ - Rotates the stack to the top
+ * @stack: Pointer to the top of the stack
+ * @line_number: Line number being executed
+ */
+void rotl_(stack_t **stack, unsigned int line_number)
+{
+	stack_t *last = NULL;
+	
+    if (*stack == NULL || (*stack)->next == NULL)
+        return;
+
+    last = *stack;
+    while (last->next != NULL)
+        last = last->next;
+
+    last->next = *stack;
+    (*stack)->prev = last;
+    *stack = (*stack)->next;
+    (*stack)->prev = NULL;
+    last->next->next = NULL;
+}
