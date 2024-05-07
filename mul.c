a49862477367e541bed5f8e1ed1be4218126194d
bRight <141350864+brightsablah@users.noreply.github.com>
2024-05-07 00:54:49 +0000

    Create mul.c

---
+++ mul.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * mul_op - Multiplies the second top element of the stack by the top element
+ * @stack: Pointer to the top of the stack
+ * @line_number: Line number being executed
+ */
+void mul_op(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    (*stack)->next->n *= (*stack)->n;
+    pop(stack, line_number);
+}
