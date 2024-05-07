6dc12d3623b9b09540cb49e7559e087682f09a47
bRight <141350864+brightsablah@users.noreply.github.com>
2024-05-07 01:24:53 +0000

    Update pstr.c

--- pstr.c
+++ pstr.c
@@ -1,14 +1,13 @@
 #include "monty.h"
 
 /**
- * pstr_op - Prints the string starting at the top of the stack
+ * pstr_ - Prints the string starting at the top of the stack
  * @stack: Pointer to the top of the stack
  * @line_number: Line number being executed
  */
 void pstr_(stack_t **stack, unsigned int line_number)
 {
     stack_t *current = *stack;
-
     (void)line_number;
 
     while (current != NULL && current->n != 0 && current->n >= 0 && current->n <= 127)
